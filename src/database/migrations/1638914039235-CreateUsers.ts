764665ac278bf1c87482f0dc9f9781052221d402
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-08 17:06:16 -0400

    corrige a chave primaria de users

--- src/database/migrations/1638914039235-CreateUsers.ts
+++ src/database/migrations/1638914039235-CreateUsers.ts
@@ -10,6 +10,7 @@ export class CreateUsers1638914039235 implements MigrationInterface {
                     {
                         name: "id",
                         type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
