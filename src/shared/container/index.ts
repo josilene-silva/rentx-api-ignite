de795d4675efd1dc3b70d46f85568387f5ad029a
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-07 22:58:51 -0400

    adiciona migration, entity, repository, create  usecase, controller e router da tabela users

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository", // nome do container
+    UsersRepository // classe referenciada
+);
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository", // nome do container
