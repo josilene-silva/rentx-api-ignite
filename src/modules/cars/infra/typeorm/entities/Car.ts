97409bf9404a9f015d7da3865623b41e4a0bec37
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-10 20:53:17 -0400

    ajusta entity, cria repository, use case, controller e route de cars

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,5 +1,13 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "./Category";
 
 @Entity("cars")
 class Car {
@@ -27,8 +35,9 @@ class Car {
   @Column()
   brand: string;
 
-  @Column()
-  category_id: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category | string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -37,7 +46,6 @@ class Car {
     if (!this.id) {
       this.id = uuidV4();
       this.available = true;
-      this.created_at = new Date();
     }
   }
 }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
@@ -22,7 +23,4 @@ container.registerSingleton<ISpecificationsRepository>(
   SpecificationsRepository // classe referenciada
 );
 
-// container.registerSingleton<ICarsRepository>(
-//   "CarsRepository",
-//   CarsRepository
-// )
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
