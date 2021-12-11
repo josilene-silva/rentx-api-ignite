b7640c4ba52e654d98d3cbe1878a632113a9adb0
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-10 21:43:02 -0400

    corrige conecao do host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -14,7 +14,7 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 //   });
 // });
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (host = "localhost"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
