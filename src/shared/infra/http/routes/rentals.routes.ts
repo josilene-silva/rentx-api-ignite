1eb009a373c025bed53e7adba71533af1888eebc
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-13 03:31:41 -0400

    implementa create rental

---
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { CreateRentalController } from "@modules/rentals/useCase/createRental/CreateRentalController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
+const rentalsRoutes = Router();
+const createRentalController = new CreateRentalController();
+
+rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+
+export { rentalsRoutes };
