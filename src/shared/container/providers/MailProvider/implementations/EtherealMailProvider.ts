7fd3c8eddf4adaf5ec18b40a7501b501151fc4ee
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2021-12-14 17:53:37 -0400

    cria template para envio de email

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,5 +1,7 @@
 import { injectable } from "tsyringe";
 import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
 
 import { IMailProvider } from "../IMailProvider";
 
@@ -26,13 +28,23 @@ class EtherealMailProvider implements IMailProvider {
       .catch((err) => console.error(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHtml = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreplay@rentx.com.br>",
       subject,
-      text: body,
-      html: body,
+      html: templateHtml,
     });
 
     console.log("Message sent: ", message.messageId);
